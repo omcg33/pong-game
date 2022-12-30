34a7c5a4122c02e80de689bd8099d85d731c24b0
Alexey Chimochikhin <alexey.chimochikhin@sravni.ru>
2022-12-30 16:57:25 +0300

    remove unused

--- js/consts.js
+++
@@ -1,4 +0,0 @@
-export const DIRECTION = {
-    FORWARD: 'forward',
-    BACKWARD: 'backward',
-}
--- js/objects/scoreTrigger.js
+++
@@ -1,9 +0,0 @@
-import { Wall } from './wall.js';
-
-export class ScoreTrigger extends Wall {
-    constructor(player, settings) {
-       super(settings)
- 
-       this.player = player;
-    }
-}
